4f37ef9fb4a874c4382caf2d93a01a03dfc22e25
Hazel OHearn <gygaxis@protonmail.com>
2021-02-18 15:35:15 -0400

    added some integration testing

---
+++ zcashrpc-typegen/tests/hollistic_tests.rs
@@ -0,0 +1,25 @@
+#[test]
+fn validate_basic_struct_test_output() {
+    call_test("basic_struct");
+}
+
+fn call_test(test_name: &str) {
+    assert!(std::process::Command::new("cargo")
+        .args(&[
+            "run",
+            &format!("./test_data/{}", test_name),
+            &format!("test_output/{}.rs", test_name),
+        ])
+        .output()
+        .expect("cargo run failed")
+        .status
+        .success());
+
+    let output =
+        std::fs::read_to_string(format!("./test_output/{}.rs", test_name));
+    let expected = std::fs::read_to_string(format!(
+        "./test_output/{}_expected.rs",
+        test_name
+    ));
+    assert_eq!(output.unwrap(), expected.unwrap());
+}
