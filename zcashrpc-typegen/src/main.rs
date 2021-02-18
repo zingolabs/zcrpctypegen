c30b8a653299ed6842ab33d044cc693876e982ea
Hazel OHearn <gygaxis@protonmail.com>
2021-02-18 17:18:35 -0400

    quote_value refactor, fixed typo

--- zcashrpc-typegen/src/main.rs
+++ zcashrpc-typegen/src/main.rs
@@ -1,3 +1,5 @@
+type GenericResult<T> = Result<T, Box<dyn std::error::Error>>;
+
 fn main() {
     std::fs::File::create(output_path()).unwrap();
     for filenode in std::fs::read_dir(&std::path::Path::new(
@@ -38,9 +40,7 @@ fn output_path() -> Box<std::path::Path> {
     ))
 }
 
-fn get_data(
-    file: &std::fs::DirEntry,
-) -> Result<serde_json::Value, Box<dyn std::error::Error>> {
+fn get_data(file: &std::fs::DirEntry) -> GenericResult<serde_json::Value> {
     let mut file = std::fs::File::open(file.path())?;
     let mut file_body = String::new();
     use std::io::Read as _;
@@ -52,7 +52,7 @@ fn get_data(
 fn typegen(
     data: serde_json::Map<String, serde_json::Value>,
     name: &str,
-) -> Result<(), Box<dyn std::error::Error>> {
+) -> GenericResult<()> {
     let mut code = Vec::new();
     // The default collection behind a serde_json_map is a BTreeMap
     // and being the predicate of "in" causes into_iter to be called.
@@ -63,7 +63,7 @@ fn typegen(
             &field_name,
             proc_macro2::Span::call_site(),
         );
-        let val = quote_value(Some(&to_camel_case(&field_name)), val)?;
+        let val = quote_value(&to_camel_case(&field_name), val)?;
         let added_code = quote::quote!(pub #key: #val,);
         code.push(added_code);
     }
@@ -87,17 +87,14 @@ fn typegen(
     Ok(())
 }
 
-fn alias(
-    data: serde_json::Value,
-    name: &str,
-) -> Result<(), Box<dyn std::error::Error>> {
+fn alias(data: serde_json::Value, name: &str) -> GenericResult<()> {
     if let serde_json::Value::Object(_) = data {
         unimplemented!("We don't want to create struct aliases.")
     }
-    let name = proc_macro2::Ident::new(&name, proc_macro2::Span::call_site());
-    let type_body = quote_value(None, data)?;
+    let ident = proc_macro2::Ident::new(&name, proc_macro2::Span::call_site());
+    let type_body = quote_value(&to_camel_case(name), data)?;
     let aliased = quote::quote!(
-        pub type #name = #type_body;
+        pub type #ident = #type_body;
     );
     //println!("Going to write: {}", aliased.to_string());
     let mut output = std::fs::OpenOptions::new()
@@ -111,50 +108,52 @@ fn alias(
 }
 
 fn quote_value(
-    name: Option<&str>,
+    name: &str,
     val: serde_json::Value,
-) -> Result<proc_macro2::TokenStream, Box<dyn std::error::Error>> {
-    Ok(match val {
-        serde_json::Value::String(kind) => match kind.as_str() {
-            "Decimal" => quote::quote!(rust_decimal::Decimal),
-            "bool" => quote::quote!(bool),
-            "String" => quote::quote!(String),
-            otherwise => {
-                return Err(format!(
-                    "Unexpected type descriptor: \n {}",
-                    otherwise
-                )
-                .into())
-            }
-        },
+) -> GenericResult<proc_macro2::TokenStream> {
+    match val {
+        serde_json::Value::String(kind) => quote_terminal(kind.as_str()),
+        serde_json::Value::Array(vec) => quote_array(name, vec),
+        serde_json::Value::Object(obj) => quote_object(name, obj),
+        otherwise => {
+            Err(format!("Did not expect to recieve: \n {}", otherwise).into())
+        }
+    }
+}
 
-        serde_json::Value::Array(mut vec) => {
-            let val = quote_value(
-                name,
-                vec.pop().ok_or(<Box<dyn std::error::Error>>::from(
-                    String::from("Cannot determine type of empty array"),
-                ))?,
-            )?;
-            quote::quote!(Vec<#val>)
-        }
-        serde_json::Value::Null => {
-            return Err(String::from("Unexpected null value").into())
-        }
-        serde_json::Value::Object(obj) => {
-            let name = name.ok_or(<Box<dyn std::error::Error>>::from(
-                format!("Received struct with no name: {:#?}", obj),
-            ))?;
-            typegen(obj, name)?;
-            let ident =
-                proc_macro2::Ident::new(name, proc_macro2::Span::call_site());
-            quote::quote!(#ident)
-        }
+fn quote_terminal(val: &str) -> GenericResult<proc_macro2::TokenStream> {
+    Ok(match val {
+        "Decimal" => quote::quote!(rust_decimal::Decimal),
+        "bool" => quote::quote!(bool),
+        "String" => quote::quote!(String),
         otherwise => {
             return Err(
-                format!("Did not expect to recieve: \n {}", otherwise).into()
+                format!("Unexpected type descriptor: \n {}", otherwise).into()
             )
         }
     })
+}
+
+fn quote_array(
+    name: &str,
+    mut array_of: Vec<serde_json::Value>,
+) -> GenericResult<proc_macro2::TokenStream> {
+    let val = quote_value(
+        name,
+        array_of.pop().ok_or(<Box<dyn std::error::Error>>::from(
+            String::from("Cannot determine type of empty array"),
+        ))?,
+    )?;
+    Ok(quote::quote!(Vec<#val>))
+}
+
+fn quote_object(
+    name: &str,
+    val: serde_json::Map<String, serde_json::Value>,
+) -> GenericResult<proc_macro2::TokenStream> {
+    typegen(val, name)?;
+    let ident = proc_macro2::Ident::new(name, proc_macro2::Span::call_site());
+    Ok(quote::quote!(#ident))
 }
 
 fn to_camel_case(input: &str) -> String {
@@ -170,7 +169,8 @@ mod unit {
 
     #[test]
     fn quote_value_string() {
-        let quoted_string = quote_value(None, serde_json::json!("String"));
+        let quoted_string =
+            quote_value("some_field", serde_json::json!("String"));
         assert_eq!(
             quote::quote!(String).to_string(),
             quoted_string.unwrap().to_string(),
@@ -178,7 +178,8 @@ mod unit {
     }
     #[test]
     fn quote_value_number() {
-        let quoted_number = quote_value(None, serde_json::json!("Decimal"));
+        let quoted_number =
+            quote_value("some_field", serde_json::json!("Decimal"));
         assert_eq!(
             quote::quote!(rust_decimal::Decimal).to_string(),
             quoted_number.unwrap().to_string(),
@@ -186,7 +187,7 @@ mod unit {
     }
     #[test]
     fn quote_value_bool() {
-        let quoted_bool = quote_value(None, serde_json::json!("bool"));
+        let quoted_bool = quote_value("some_field", serde_json::json!("bool"));
         assert_eq!(
             quote::quote!(bool).to_string(),
             quoted_bool.unwrap().to_string(),
