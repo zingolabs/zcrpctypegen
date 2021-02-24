da477de9389bd748102f12f6d6e7a66057a388b6
Hazel OHearn <gygaxis@protonmail.com>
2021-02-24 15:45:18 -0400

    added PartialEq error impl, conditional to testing

--- zcashrpc-typegen/src/error.rs
+++ zcashrpc-typegen/src/error.rs
@@ -1,6 +1,7 @@
 pub type TypegenResult<T> = Result<T, TypegenError>;
 
 #[derive(Debug, derive_more::From)]
+#[cfg_attr(test, derive(PartialEq))]
 pub enum TypegenError {
     Filesystem(FSError),
     InvalidJson(InvalidJsonError),
@@ -8,6 +9,7 @@ pub enum TypegenError {
 }
 
 #[derive(Debug)]
+#[cfg_attr(test, derive(PartialEq))]
 pub struct FSError {
     message: String,
     location: Box<std::path::Path>,
@@ -41,12 +43,14 @@ impl InvalidJsonError {
 }
 
 #[derive(Debug, derive_more::From)]
+#[cfg_attr(test, derive(PartialEq))]
 pub struct InvalidAnnotationError {
     pub kind: InvalidAnnotationKind,
     pub location: String,
 }
 
 #[derive(Debug)]
+#[cfg_attr(test, derive(PartialEq))]
 pub enum InvalidAnnotationKind {
     Null,
     Bool(bool),
@@ -73,9 +77,28 @@ impl From<serde_json::Value> for InvalidAnnotationKind {
 #[cfg(test)]
 mod unit {
     use super::*;
+    macro_rules! compare {
+            ($a:ident, $b:ident: $($f:ident),*) => {
+                $a.input == $b.input $(&& $a.err.$f() == $b.err.$f())*
+            }
+        }
+
+    impl PartialEq<Self> for InvalidJsonError {
+        fn eq(&self, other: &Self) -> bool {
+            compare!(self, other: line, column, classify)
+        }
+    }
     #[test]
     fn test_invalid_annotation() {
         let iak = InvalidAnnotationKind::Null;
-        let err = InvalidAnnotationError::from((iak, "foo".to_string()));
+        let expected_err =
+            InvalidAnnotationError::from((iak, "foo".to_string()));
+        let err = crate::quote_value(
+            "foo",
+            serde_json::Value::Null,
+            proc_macro2::TokenStream::new(),
+        )
+        .unwrap_err();
+        assert_eq!(TypegenError::InvalidAnnotation(expected_err), err);
     }
 }
