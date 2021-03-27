f966e033d38cdc2803afb38b11bb135d3f480cfb
Za Wilgustus <zancas@zingolabs.org>
2021-03-27 08:57:14 -0600

    remove unnecessary temp var

--- zcashrpc-typegen/src/error.rs
+++ zcashrpc-typegen/src/error.rs
@@ -90,9 +90,10 @@ mod unit {
     }
     #[test]
     fn test_invalid_annotation() {
-        let _iak = InvalidAnnotationKind::Null;
-        let expected_err =
-            QuizfaceAnnotationError::from((_iak, "foo".to_string()));
+        let expected_err = QuizfaceAnnotationError::from((
+            InvalidAnnotationKind::Null,
+            "foo".to_string(),
+        ));
         let err = crate::tokenize_value(
             "foo",
             serde_json::Value::Null,
