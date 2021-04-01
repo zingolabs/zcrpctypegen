9a828298d7cc471cf077ff4e4a6a2d5cc2b861d9
zancas <zancas@zingolabs.com>
2021-03-31 21:06:59 -0600

    hack in return of TokenStream

--- zcashrpc-macros/src/lib.rs
+++ zcashrpc-macros/src/lib.rs
@@ -8,7 +8,7 @@ use proc_macro::TokenStream;
 #[proc_macro]
 pub fn declare_all_rpc_methods(input: TokenStream) -> TokenStream {
     let input_ast = syn::parse_macro_input!(input as syn::ItemExternCrate);
-    input_ast.into()
+    quote::quote!(input_ast).into()
 }
 
 #[proc_macro]
