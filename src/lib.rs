cdf017ad4343399f2abd1bbffc3711de12a2d372
zancas <zancas@zingolabs.com>
2021-03-31 22:11:03 -0600

    POC parse crate AST

--- src/lib.rs
+++ src/lib.rs
@@ -13,3 +13,14 @@ pub use error::{Error, ResponseResult};
 
 /// The `ZecAmount` type alias is used to document where ZEC-denominated fields are used. Note that this does not represent Zatoshi-denominated units.
 pub type ZecAmount = rust_decimal::Decimal;
+
+#[cfg(test)]
+mod test {
+    #[test]
+    fn call_declare_all_rpc_methods() {
+        use zcashrpc_macros::declare_all_rpc_methods;
+        declare_all_rpc_methods!(
+            extern crate zcashrpc_api;
+        );
+    }
+}
