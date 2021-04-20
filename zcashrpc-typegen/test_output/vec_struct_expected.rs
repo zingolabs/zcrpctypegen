e8a1e1483860486b1ff7f0ae61c7e45ed34de526
Hazel OHearn <gygaxis@protonmail.com>
2021-04-20 13:34:23 -0300

    Fixed test to include element

--- zcashrpc-typegen/test_output/vec_struct_expected.rs
+++ zcashrpc-typegen/test_output/vec_struct_expected.rs
@@ -2,9 +2,9 @@
 //is in early alpha, and output is subject to change at any time.
 pub mod testdatavec {
     #[derive(Debug, serde :: Deserialize, serde :: Serialize)]
-    pub struct Testdatavec {
+    pub struct TestdatavecElement {
         pub fielda: String,
         pub fieldb: rust_decimal::Decimal,
     }
-    pub type TestdatavecResponse = Vec<Testdatavec>;
+    pub type TestdatavecResponse = Vec<TestdatavecElement>;
 }
