194c835c693748086243eacbe0ef7aade908aa66
zancas <zancas@zingolabs.org>
2021-05-21 11:58:22 -0600

    fix basic_struct test_data

--- test_output/basic_struct_expected.rs
+++ test_output/basic_struct_expected.rs
@@ -2,6 +2,8 @@
 //is in early alpha, and output is subject to change at any time.
 pub mod basicstruct {
     #[derive(Debug, serde :: Deserialize, serde :: Serialize)]
+    pub struct BasicstructArguments;
+    #[derive(Debug, serde :: Deserialize, serde :: Serialize)]
     pub struct BasicstructResponse {
         pub value_one: String,
         pub value_two: bool,
