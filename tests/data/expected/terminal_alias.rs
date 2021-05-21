8bb0d25681320444c3d86a308b56aa18b5ff59f4
zancas <zancas@zingolabs.org>
2021-05-21 13:02:02 -0600

    update expectations to reflect typegen output standard

--- tests/data/expected/terminal_alias.rs
+++ tests/data/expected/terminal_alias.rs
@@ -1,11 +1,17 @@
 //procedurally generated response types, note that zcashrpc-typegen
 //is in early alpha, and output is subject to change at any time.
 pub mod boolAlias {
+    #[derive(Debug, serde :: Deserialize, serde :: Serialize)]
+    pub struct BoolAliasArguments;
     pub type BoolAliasResponse = bool;
 }
 pub mod numberAlias {
+    #[derive(Debug, serde :: Deserialize, serde :: Serialize)]
+    pub struct NumberAliasArguments;
     pub type NumberAliasResponse = rust_decimal::Decimal;
 }
 pub mod stringAlias {
+    #[derive(Debug, serde :: Deserialize, serde :: Serialize)]
+    pub struct StringAliasArguments;
     pub type StringAliasResponse = String;
 }
