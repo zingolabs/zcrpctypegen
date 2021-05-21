c3252acaef4989ac4fee032edc52f7d6b72e7fc1
zancas <zancas@zingolabs.org>
2021-05-21 13:05:23 -0600

    updating test data files

---
+++ tests/data/expected/vec_struct.rs
@@ -0,0 +1,10 @@
+//procedurally generated response types, note that zcashrpc-typegen
+//is in early alpha, and output is subject to change at any time.
+pub mod testdatavec {
+    #[derive(Debug, serde :: Deserialize, serde :: Serialize)]
+    pub struct TestdatavecElement {
+        pub fielda: String,
+        pub fieldb: rust_decimal::Decimal,
+    }
+    pub type TestdatavecResponse = Vec<TestdatavecElement>;
+}
