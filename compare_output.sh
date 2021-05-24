d2b5a5aaa6f58a282b3e493fc327fdeca06d5424
Hazel OHearn <gygaxis@zingolabs.org>
2021-05-24 14:29:38 -0300

    created compare_output.sh which runs tests, then melds output of failing integration tests

---
+++ compare_output.sh
@@ -0,0 +1,12 @@
+file_path="./tests/data/observed/"
+cargo test
+if [ "$(ls -1 $file_path)" = '' ]; then
+    echo All tests passed! Exiting.
+else
+    echo Melding outputs of failed integration tests
+fi
+for file_name in `ls -1 $file_path`
+do
+    #echo $file_path/$file_name
+    meld $file_path/$file_name `echo $file_path/$file_name | sed s/observed/expected/` & &>/dev/null
+done
