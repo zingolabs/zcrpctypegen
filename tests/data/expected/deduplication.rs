9af0acd616376bc64df849f1d1be67992fb8484c
zancas <zancas@zingolabs.org>
2021-05-25 11:05:07 -0600

    fix borked test (used to manually test compare_output.sh)

--- tests/data/expected/deduplication.rs
+++ tests/data/expected/deduplication.rs
@@ -2,7 +2,7 @@
 //is in early alpha, and output is subject to change at any time.
 pub mod z_gettreestate {
     #[derive(Debug, serde :: Deserialize, serde :: Serialize)]
-    pub struct ZettreestateArguments(String);
+    pub struct ZGettreestateArguments(String);
     #[derive(Debug, serde :: Deserialize, serde :: Serialize)]
     pub struct Commitments {
         pub final_root: String,
